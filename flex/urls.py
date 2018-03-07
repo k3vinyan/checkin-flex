8fd50c90c3d3b2552b410ae96d66389bbca96a14
Kevin Yan <k3vinyan@gmail.com>
2018-03-06 19:16:24 -0800

    add route urlpatterns

--- flex/urls.py
+++ flex/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^routes', include('routes.urls', namespace='routes')),
     url(r'^$', include('home.urls', namespace='home')),
 ]
