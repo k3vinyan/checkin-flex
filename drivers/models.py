8c458320eda3c5688cfffa4a53f18193819a6708
Kevin Yan <k3vinyan@gmail.com>
2018-03-07 14:27:10 -0800

    create models for drivers

--- drivers/models.py
+++ drivers/models.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from routes.models import Route, Block
 # Create your models here.
+class Driver(models.Model):
+    DPID        = models.CharField(max_length=20, default=False)
+    first_name  = models.CharField(max_length=20)
+    last_name   = models.CharField(max_length=30)
+    checkin     = models.BooleanField(default=False)
+    isAssigned  = models.BooleanField(default=False)
+    shiftLength = models.CharField(max_length=20, blank=True)
+    startTime   = models.CharField(max_length=10, blank=True)
+    endTime     = models.CharField(max_length=10, blank=True)
+    isNoShow    = models.BooleanField(default=True)
+    checkout    = models.BooleanField(default=False)
+    packageScan = models.CharField(max_length=3, blank=True)
+    routingTool = models.CharField(max_length=3, blank=True)
+    route       = models.ForeignKey(Route, on_delete=models.CASCADE, null=True)
+    block       = models.ForeignKey(Block, null=True)
