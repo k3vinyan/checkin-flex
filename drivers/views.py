52448c72792a97d4d3e2a1094b787ae694e8dadb
Kevin Yan <k3vinyan@gmail.com>
2018-03-06 20:04:14 -0800

    create views with index route

--- drivers/views.py
+++ drivers/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from http.django import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render('drivers/index.html')
