ec399e9ca5e28cdbf4a7fcdb6be9531d90ae0da5
Kevin Yan <k3vinyan@gmail.com>
2018-03-06 20:17:00 -0800

    add drivers, checkin, assignRoutes, and checkout url patterns in main urls settings

--- flex/urls.py
+++ flex/urls.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^routes', include('routes.urls', namespace='routes')),
+    url(r'^routes/', include('routes.urls', namespace='routes')),
+    url(r'^drivers/', include('drivers.urls', namespace='routes')),
+    url(r'^checkin/', include('checkin.urls', namespace='routes')),
+    url(r'^assignRoutes/', include('assignRoutes.urls', namespace='routes')),
+    url(r'^checkout/', include('routes.urls', namespace='routes')),
     url(r'^$', include('home.urls', namespace='home')),
 ]
