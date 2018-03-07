a711c4b61e340051edeab15bc2b231c814cbe42d
Kevin Yan <k3vinyan@gmail.com>
2018-03-06 19:48:10 -0800

    update index in routes

--- routes/views.py
+++ routes/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is the route page")
+    return render(request, 'routes/index.html')
