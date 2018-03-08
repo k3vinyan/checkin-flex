1d3d192acbf10b119ebb6622f5fccd18539334bc
Kevin Yan <k3vinyan@gmail.com>
2018-03-07 17:27:47 -0800

    add migrations folders and files

--- routes/models.py
+++ routes/models.py
@@ -4,3 +4,28 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Route(models.Model):
+     route       = models.CharField(max_length=15)
+     cluster     = models.CharField(max_length=15)
+     isAssigned  = models.BooleanField(default=False)
+     tbaCount    = models.CharField(max_length=15, blank=True)
+     DP          = models.CharField(max_length=15, blank=True, null=True)
+
+     class Meta:
+         ordering = ('cluster', 'route',)
+
+class Block(models.Model):
+    date        = models.DateTimeField(max_length=20, blank=True, null=True)
+    startTime   = models.CharField(max_length=10)
+    endTime     = models.CharField(max_length=10)
+    shiftLength = models.CharField(max_length=10)
+    create_at   = models.DateTimeField(auto_now_add=True)
+
+
+class Tba(models.Model):
+    driver      = models.CharField(max_length=15, blank=True)
+    route       = models.ForeignKey(Route, on_delete=models.CASCADE, null=True)
+    tba         = models.CharField(max_length=15)
+    status      = models.CharField(max_length=15)
+    link        = models.CharField(max_length=50)
+    address     = models.CharField(max_length=50, blank=True)
