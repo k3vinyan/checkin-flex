8a18cdd6906e3347f12b82f21c61de7cbfbc18b7
Kevin Yan <k3vinyan@gmail.com>
2018-03-10 06:31:48 -0800

    add sesssion Authentication and fix error where route is set to none

--- assignRoutes/views.py
+++ assignRoutes/views.py
@@ -3,10 +3,68 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from flex import sessionHelper
+from drivers.models import Driver
+from routes.models import Route, Tba
 # Create your views here.
 def index(request):
-    if request.method == 'GET':
+    drivers = Driver.objects.filter(checkin=True, checkout=False)
+    routes = Route.objects.filter(isAssigned=False)
+    DP = {'drivers': drivers, 'routes': routes }
+    if request.method == "GET":
         if sessionHelper.isAuthSession():
-            return render(request, 'assignRoutes/index.html')
+            drivers = Driver.objects.filter(checkin=True, checkout=False)
+            routes = Route.objects.filter(isAssigned=False)
+            DP = {'drivers': drivers, 'routes': routes }
+            return render(request, 'assignRoutes/index.html', {'DP': DP})
         else:
             return redirect('../')
+    if request.method == "POST":
+
+        route = request.POST.get('route')
+        driverId = request.POST.get('driverId')
+
+        driver = Driver.objects.get(id=driverId)
+        if route == "None":
+            print "route is none"
+        else:
+            route = Route.objects.get(id=route)
+
+        #remove Driver from Route Model
+        if driver.route != None:
+            try:
+                oldRoute = driver.route
+                newRoute = Route.objects.get(id=oldRoute.id)
+                newRoute.isAssigned = False
+                newRoute.driver = None
+                newRoute.save(update_fields=['isAssigned'])
+                newRoute.save(update_fields=['DP'])
+            except ObjectDoesNotExist:
+                print "does not exist"
+
+        #if route is None sit driver route to None
+        if route == "None":
+            driver.route = None
+        else:
+            driver.route = route
+            r = Route.objects.get(id=route.id)
+            first = driver.first_name
+            second = driver.last_name
+            r.DP = first + " " + second
+            r.isAssigned = True
+            r.save(update_fields=['isAssigned'])
+            r.save(update_fields=['DP'])
+            tbas = Tba.objects.filter(route=r)
+            for tba in tbas:
+                tba.driver = driver
+                tba.save(update_fields=['driver'])
+        driver.isAssigned = True
+        driver.save(update_fields=['route'])
+        driver.save(update_fields=['isAssigned'])
+
+
+
+        drivers = Driver.objects.filter(checkin=True, checkout=False)
+
+        routes = Route.objects.filter(isAssigned=False)
+
+        return render(request, 'assignRoutes/index.html', {'DP': DP})
