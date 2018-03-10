fc1977e113cce60f20512b179a7d1b39c4369856
Kevin Yan <k3vinyan@gmail.com>
2018-03-10 03:53:43 -0800

    add foreign key to Tbas model

--- routes/models.py
+++ routes/models.py
@@ -1,31 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
 class Route(models.Model):
-     route       = models.CharField(max_length=15)
-     cluster     = models.CharField(max_length=15)
+     route       = models.CharField(max_length=50)
+     cluster     = models.CharField(max_length=50)
      isAssigned  = models.BooleanField(default=False)
-     tbaCount    = models.CharField(max_length=15, blank=True)
-     DP          = models.CharField(max_length=15, blank=True, null=True)
+     tbaCount    = models.CharField(max_length=50, blank=True)
+     DP          = models.CharField(max_length=50, blank=True, null=True)
 
      class Meta:
          ordering = ('cluster', 'route',)
 
 class Block(models.Model):
-    date        = models.DateTimeField(max_length=20, blank=True, null=True)
-    startTime   = models.CharField(max_length=10)
-    endTime     = models.CharField(max_length=10)
-    shiftLength = models.CharField(max_length=20)
+    date        = models.DateTimeField(max_length=50, blank=True, null=True)
+    startTime   = models.CharField(max_length=50)
+    endTime     = models.CharField(max_length=50)
+    shiftLength = models.CharField(max_length=50)
     create_at   = models.DateTimeField(auto_now_add=True)
 
 
 class Tba(models.Model):
-    driver      = models.CharField(max_length=15, blank=True)
+    driver      = models.ForeignKey('drivers.Driver', blank=True, null=True)
     route       = models.ForeignKey(Route, on_delete=models.CASCADE, null=True)
-    tba         = models.CharField(max_length=15)
-    status      = models.CharField(max_length=15)
-    link        = models.CharField(max_length=60)
-    address     = models.CharField(max_length=30)
+    tba         = models.CharField(max_length=50)
+    status      = models.CharField(max_length=50)
+    link        = models.CharField(max_length=100)
+    address     = models.CharField(max_length=100)
