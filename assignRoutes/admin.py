70ac09a82c3f25bdbb74012c038a1f4d4ed46b96
Kevin Yan <k3vinyan@gmail.com>
2018-03-20 21:35:20 -0700

    create superuser

--- assignRoutes/admin.py
+++ assignRoutes/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from drivers.models import Driver
+from routes.models import Route, Block, Tba
 
 # Register your models here.
+admin.site.register(Driver)
+admin.site.register(Route)
+admin.site.register(Block)
+admin.site.register(Tba)
