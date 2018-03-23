4e821b556c3e906bac4e2e2ec474aefb6be96884
Kevin Yan <k3vinyan@gmail.com>
2018-03-23 12:26:40 -0700

    remove isAuthSession

--- drivers/views.py
+++ drivers/views.py
@@ -13,66 +13,64 @@ from django.core.exceptions import ObjectDoesNotExist
 def index(request):
 
     if request.method == 'GET':
-        if sessionHelper.isAuthSession():
+        #this is a test roster, todo
+        session = requests.Session()
 
-            #this is a test roster, todo
-            session = requests.Session()
-
-            #test roster
-            #s = session.get("http://localhost:8000/drivers/roster", headers=sessionHelper.headers)
-            rosterUrl = sessionHelper.getRosterUrl()
-            s = session.get(rosterUrl, headers=sessionHelper.headers)
-            BSObj = BeautifulSoup(s.text, 'lxml')
-            daIDs   = BSObj.find(id="cspDATable").find_all(attrs={'data-bind': 'text: transporterId'})
-            daNames = BSObj.find(id="cspDATable").find_all(attrs={'data-bind': 'text: DAName'})
-            shiftLengths = BSObj.find(id="cspDATable").find_all(attrs={'data-bind': 'text: duration'})
-            startTimes = BSObj.find(id="cspDATable").find_all(attrs={'data-bind': 'text: startTime'})
-            endTimes = BSObj.find(id="cspDATable").find_all(attrs={'data-bind': 'text: endTime'})
+        #test roster
+        #s = session.get("http://localhost:8000/drivers/roster", headers=sessionHelper.headers)
+        rosterUrl = sessionHelper.getRosterUrl()
+        s = session.get(rosterUrl, headers=sessionHelper.headers)
+        BSObj = BeautifulSoup(s.text, 'lxml')
+        daIDs   = BSObj.find(id="cspDATable").find_all(attrs={'data-bind': 'text: transporterId'})
+        daNames = BSObj.find(id="cspDATable").find_all(attrs={'data-bind': 'text: DAName'})
+        shiftLengths = BSObj.find(id="cspDATable").find_all(attrs={'data-bind': 'text: duration'})
+        startTimes = BSObj.find(id="cspDATable").find_all(attrs={'data-bind': 'text: startTime'})
+        endTimes = BSObj.find(id="cspDATable").find_all(attrs={'data-bind': 'text: endTime'})
 
 
-            for iD, name, sl, st, et  in zip(daIDs, daNames, shiftLengths, startTimes, endTimes):
-                name = name.text.split(" ")
-                firstName = name[0]
-                lastName = name[1]
-                try:
-                    Block.objects.get(startTime=st.text)
-                except:
-                    block = Block(startTime=st.text, endTime=et.text, shiftLength=sl.text)
-                    block.save()
-                try:
-                    Driver.objects.get(DPID=iD.text)
-                except ObjectDoesNotExist:
-                    block = Block.objects.get(startTime=st.text)
-                    driver = Driver(DPID=iD.text,
-                                    first_name = firstName,
-                                    last_name = lastName,
-                                    shiftLength = sl.text,
-                                    startTime = st.text,
-                                    endTime = et.text,
-                                    block = block
-                                    )
-                    driver.save()
+        for iD, name, sl, st, et  in zip(daIDs, daNames, shiftLengths, startTimes, endTimes):
+            name = name.text.split(" ")
+            firstName = name[0]
+            lastName = name[1]
+            try:
+                Block.objects.get(startTime=st.text)
+            except:
+                block = Block(startTime=st.text, endTime=et.text, shiftLength=sl.text)
+                block.save()
+            try:
+                Driver.objects.get(DPID=iD.text)
+            except ObjectDoesNotExist:
+                block = Block.objects.get(startTime=st.text)
+                driver = Driver(DPID=iD.text,
+                                first_name = firstName,
+                                last_name = lastName,
+                                shiftLength = sl.text,
+                                startTime = st.text,
+                                endTime = et.text,
+                                block = block
+                                )
+                driver.save()
 
 
-            if request.method == 'GET':
-                blocks = Block.objects.order_by('startTime').all()
-                drivers = Driver.objects.order_by('startTime', 'first_name').all()
+        if request.method == 'GET':
+            blocks = Block.objects.order_by('startTime').all()
+            drivers = Driver.objects.order_by('startTime', 'first_name').all()
 
-                total = {}
+            total = {}
 
-                for driver in drivers:
-                    for block in blocks:
-                        if block.startTime == driver.startTime:
-                                try:
-                                    total[block.startTime]
-                                except KeyError:
-                                    total[block.startTime] = 0
-                                total[block.startTime] += 1
+            for driver in drivers:
+                for block in blocks:
+                    if block.startTime == driver.startTime:
+                            try:
+                                total[block.startTime]
+                            except KeyError:
+                                total[block.startTime] = 0
+                            total[block.startTime] += 1
 
 
-                return render(request, 'drivers/index.html', {'drivers':drivers, 'blocks':blocks, 'total':total})
-        else:
-            return redirect('../')
+            return render(request, 'drivers/index.html', {'drivers':drivers, 'blocks':blocks, 'total':total})
+    else:
+        return redirect('../')
 
 
 
